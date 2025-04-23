daa2378f58e325363b5b9d811d0d910cccb99e02
Damaris Ngari <wanjirungari2@gmail.com>
2025-04-23 18:56:46 +0300

    fix: Update SQLAlchemy queries to 2.0 syntax in app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from models import db, Restaurant, Pizza, RestaurantPizza
+from flask_migrate import Migrate
+
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -9,6 +11,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
 
 @app.route('/')
 def index():
